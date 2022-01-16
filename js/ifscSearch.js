35817c7192cbe12f4b604c6569d8646fda26ba07
Nathaniel Nunes <manutdkid77@gmail.com>
2022-01-16 19:34:30 +0530

    query razorpay endpoint to get ifsc details

--- js/ifscSearch.js
+++ js/ifscSearch.js
@@ -7,4 +7,15 @@ ifscForm.addEventListener("submit", (e) => {
   const ifscCode = ifscTextField.value;
 
   if (ifscCode.length === 0) return;
+
+  getIfscCodeDetails(ifscCode);
 });
+
+function getIfscCodeDetails(ifscCode) {
+  fetch(`https://ifsc.razorpay.com/${ifscCode}`)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((err) => {
+      console.error(err);
+    });
+}
